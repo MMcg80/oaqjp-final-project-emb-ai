645e9c173ed16b52d710f3c6abc08df8d175a61c
MMCg80 <shirokuma028@gmail.com>
2026-04-24 21:54:09 -0400

    task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -13,5 +13,32 @@ def emotion_detector(text_to_analyze):
 
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
-    
-    return response.text
+
+    # If the response status code is 200, extract emotions and their score from the response
+    if response.status_code == 200:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)
+        
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        
+        dominant_emotion = max(emotions, key=emotions.get)
+
+
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
+	
+
+# result = emotion_detector("I am so happy this . works!")
+# print(result['dominant_emotion']) 
